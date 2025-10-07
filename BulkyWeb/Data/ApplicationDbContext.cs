c7c155b030247d5db76d058573ab0e9b5b41ec16
Kincaid. Andrew (Enterprise Services) <andrew.kincaid@esb.ie>
2025-10-07 09:58:56 +0100

    ASP.NEt basic create, read, display Categories in Database using Entity Framework .Net 8

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    //DbContext used Entity Framework COre
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+                
+        }
+
+        //this will create a table called Categories based on Category Model
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                 new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                  new Category { Id = 3, Name = "Comedy", DisplayOrder = 3 });
+        }
+
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,12 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options 
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));//this get the con string from app settings
 
 var app = builder.Build();
 
