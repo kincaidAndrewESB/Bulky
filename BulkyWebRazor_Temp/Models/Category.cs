a6176201ff413fc6d03a4a04199e61fc6715f3f0
Kincaid. Andrew (Enterprise Services) <andrew.kincaid@esb.ie>
2026-03-23 16:53:50 +0000

    Added Temp BulkyWebRazor Project to the solution

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key] //not required as Id will be recognised as Primary Key
+        public int Id { get; set; }
+        [Required]//data anotation
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The field Display Order must be between 1 and 100")]//tag helpers
+        public int DisplayOrder { get; set; }
+    }
+}
