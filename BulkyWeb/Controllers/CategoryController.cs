97f5dcb74b5ee02c1eeba54317ca65ae9646ddcf
Kincaid. Andrew (Enterprise Services) <andrew.kincaid@esb.ie>
2026-03-23 12:00:11 +0000

    Initial push - Catergory CRUD operations and Notifications working Section 2 completed

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -31,7 +31,7 @@ namespace BulkyWeb.Controllers
             //Additional Input Validation options - these are server side and require a reload
             if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
             }
             //if (obj.Name != null &&obj.Name.ToLower() == "test")
             //{
@@ -43,10 +43,83 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();//this executes the database update when you are ready
+                TempData["Success"] = "Category created successfully!";
                 return RedirectToAction("Index");
             }
             return View();
             
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id.Value == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c=>c.Name.Contains("Sci"));
+            Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            //Category objCategory = _db.Categories.FirstOrDefault( c => c.Id == id.Value);
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //Additional Input Validation options - these are server side and require a reload
+            if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+             //Use JavaScript to carry out client side validation is more efficient*/
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();//this executes the database update when you are ready
+                TempData["Success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id.Value == 0)
+            {
+                return NotFound();
+            }
+           Category? categoryFromDb = _db.Categories.Find(id);
+           if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            //Category objCategory = _db.Categories.FirstOrDefault( c => c.Id == id.Value);
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully!";
+
+            return RedirectToAction("Index");
         }
     }
 }
--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -19,8 +19,8 @@ namespace BulkyWeb.Data
             //base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
-                 new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
-                  new Category { Id = 3, Name = "Comedy", DisplayOrder = 3 });
+                new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Comedy", DisplayOrder = 3 });
         }
 
     }
